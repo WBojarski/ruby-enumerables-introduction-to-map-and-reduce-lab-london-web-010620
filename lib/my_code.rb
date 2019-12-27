2b65437b37894efa8ef4a73038026ee8c2e445be
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-27 07:50:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# My Code here....
+def map_to_negativize(array)
+	array.map { |n| n * -1 }
+
+end
+
+def map_to_no_change(array)
+
+	array.map { |n| n  }
+
+end
+
+def map_to_double(array)
+	array.map { |n| n * 2 }
+end
+
+def map_to_square(array)
+
+	array.map { |n| n * n }
+end
+
+def reduce_to_total(array, starting_point = 0)
+
+	array.reduce(starting_point) {|sum, n| sum + n}
+
+end
+
+
